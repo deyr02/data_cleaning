db21b8085eb6b7138a45b64813e34de2173b9910
Rajib Dey <Rajib Dey@RAJIB>
2017-08-11 13:59:58 +1200

    Add project files.

---
+++ dataCleaning_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using System.Data;
+using System.Collections;
+
+namespace dataCleaning_3
+{
+    class Program
+    {
+        public ArrayList ReadFile( String filepath)
+        {
+            ArrayList list = new ArrayList();
+            string path = filepath;
+            using (TextFieldParser parser = new TextFieldParser(path))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    string Data = parser.ReadLine();
+                    list.Add(Data);
+                    Console.WriteLine(Data);
+
+
+                }
+                Console.ReadKey();
+            }
+            return list;
+        }
+        
+        static void Main(string[] args)
+        {
+            Program F = new Program();
+           // string v = "D:\My Study\BCS S5\Data Mining\Assignment - 1\dataCleaning_3\dataCleaning_3\step_2.csv";
+            F.ReadFile("step_2.csv");
+
+        }
+    }
+}
